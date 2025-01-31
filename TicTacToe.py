e9d78bcba34900dfed0672e2b39f2dce1fd7c688
Fynn8962 <142886149+Fynn8962@users.noreply.github.com>
2025-01-31 16:31:08 +0100

    Final

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,107 +1,137 @@
 import random
 
+playerPoints = 0
+computerPoints = 0
 
-spots = {
-    1: "1",
-    2: "2",
-    3: "3",
-    4: "4",
-    5: "5",
-    6: "6",
-    7: "7",
-    8: "8",
-    9: "9"
-}
+print("Spiel geht bis Drei Punkte")
 
-playerWon = False
+while playerPoints < 3 and computerPoints < 3:
+        
+    spots = {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+        6: "6",
+        7: "7",
+        8: "8",
+        9: "9"
+    }
 
-
-def print_board():
-    board = (f"|{spots[1]}|{spots[2]}|{spots[3]}|\n"
-             f"|{spots[4]}|{spots[5]}|{spots[6]}|\n"
-             f"|{spots[7]}|{spots[8]}|{spots[9]}|")
-    print(board)
-
-
-def check_winner():
-    global playerWon
-    if (spots[1] == spots[2] == spots[3] or
-        spots[4] == spots[5] == spots[6] or
-            spots[7] == spots[8] == spots[9]):
-        playerWon = True
-
-    elif (spots[1] == spots[4] == spots[7] or
-          spots[2] == spots[5] == spots[8] or
-          spots[3] == spots[6] == spots[9]):
-        playerWon = True
-
-    elif (spots[1] == spots[5] == spots[9] or
-          spots[3] == spots[5] == spots[7]):
-        playerWon = True
+    playerWon = False
 
 
-print_board()
+    def print_board():
+        board = (f"|{spots[1]}|{spots[2]}|{spots[3]}|\n"
+                f"|{spots[4]}|{spots[5]}|{spots[6]}|\n"
+                f"|{spots[7]}|{spots[8]}|{spots[9]}|")
+        print(board)
 
-while playerWon == False:
 
-    playerPlayed = False
+    def check_winner():
+        global playerWon
+        global threePointsReached
+        if (spots[1] == spots[2] == spots[3] or
+            spots[4] == spots[5] == spots[6] or
+                spots[7] == spots[8] == spots[9]):
+            playerWon = True
+            
 
-    while playerPlayed == False:
+        elif (spots[1] == spots[4] == spots[7] or
+            spots[2] == spots[5] == spots[8] or
+            spots[3] == spots[6] == spots[9]):
+            playerWon = True
 
-        playerInput = input("wähle Feld -> ")
 
-        if (playerInput.isdigit() and int(playerInput) <= 9 and int(playerInput) >= 1):
-            if (spots[int(playerInput)] == "x" or spots[int(playerInput)] == "*"):
-                print("Feld bereits besetzt")
-                continue
-            else:
-                break
-        else:
-            print("ungültige Eingabe")
-            continue
+        elif (spots[1] == spots[5] == spots[9] or
+            spots[3] == spots[5] == spots[7]):
+            playerWon = True
 
-    match playerInput:
-        case "1":
-            spots[1] = "x"
-        case "2":
-            spots[2] = "x"
-        case "3":
-            spots[3] = "x"
-        case "4":
-            spots[4] = "x"
-        case "5":
-            spots[5] = "x"
-        case "6":
-            spots[6] = "x"
-        case "7":
-            spots[7] = "x"
-        case "8":
-            spots[8] = "x"
-        case "9":
-            spots[9] = "x"
+    def check_tie():
+        feldbesetzt = 0
+        for spot in spots:
+            if spots[spot] == "x" or spots[spot] == "*":
+                feldbesetzt += 1 
+        if feldbesetzt == 9:
+            return True
+        return False
 
-    print("Dein Spielzug: ")
+
     print_board()
-    check_winner()
 
-    if (playerWon == True):
-        print("Spieler hat gewonnen!")
-        break
+    while playerWon == False:
 
-    botPlayed = False
-    while (botPlayed == False):
-        randomNum = random.randint(1, 9)
+        playerPlayed = False
 
-        if (spots[randomNum] == "x" or spots[randomNum] == "*"):
-            continue
-        else:
-            spots[randomNum] = "*"
-            botPlayed = True
-            print("Computer Spielzug: ")
+        while playerPlayed == False:
+
+            playerInput = input("wähle Feld -> ")
+
+            if (playerInput.isdigit() and int(playerInput) <= 9 and int(playerInput) >= 1):
+                if (spots[int(playerInput)] == "x" or spots[int(playerInput)] == "*"):
+                    print("Feld bereits besetzt")
+                    continue
+                else:
+                    break
+            else:
+                print("ungültige Eingabe")
+                continue
+
+        match playerInput:
+            case "1":
+                spots[1] = "x"
+            case "2":
+                spots[2] = "x"
+            case "3":
+                spots[3] = "x"
+            case "4":
+                spots[4] = "x"
+            case "5":
+                spots[5] = "x"
+            case "6":
+                spots[6] = "x"
+            case "7":
+                spots[7] = "x"
+            case "8":
+                spots[8] = "x"
+            case "9":
+                spots[9] = "x"
+
+        print("Dein Spielzug: ")
+        print_board()
+        check_winner()
+        check_tie()
+        if check_tie():
+            print("Unentschieden")
+            break
+        if (playerWon == True):
+            playerPoints += 1
+            print("Spieler hat gewonnen!")
+            print("Spieler hat " + str(playerPoints) + " Punkte")
             break
 
-    print_board()
-    check_winner()
-    if (playerWon == True):
-        print("Computer hat gewonnen!")
-        break
+        botPlayed = False
+        while (botPlayed == False):
+            randomNum = random.randint(1, 9)
+
+            if (spots[randomNum] == "x" or spots[randomNum] == "*"):
+                continue
+            else:
+                spots[randomNum] = "*"
+                botPlayed = True
+                print("Computer Spielzug: ")
+                break
+
+        print_board()
+        check_winner()
+        check_tie()
+        if check_tie():
+            print("Unentschieden")
+            break
+        if (playerWon == True):
+            computerPoints += 1
+            print("Computer hat gewonnen!")
+            print("Computer hat " + str(computerPoints) + " Punkte")
+
+            break
